baa6acb52d07078b187dd21550636864014ef72c
2362696606 <47619616+2362696606@users.noreply.github.com>
2020-05-04 10:04:16 +0800

    1.0.2

--- p6/DemoB/DemoB/main.cpp
+++ p6/DemoB/DemoB/main.cpp
@@ -1,21 +1,82 @@
 #include <Windows.h>
 
+HINSTANCE hinst;
+
 LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	//定义窗口类
 	WNDCLASSEX wcx;
+	HWND hwnd;
+	MSG msg;
+	BOOL fGotMessage;
 
-	wcx.lpszClassName = "MainWClass";
+	hinst = hinstance;
+
+	wcx.cbSize = sizeof(wcx);
+	wcx.style = CS_HREDRAW | CS_VREDRAW;
+	wcx.lpfnWndProc = MainWndProc;
+	wcx.cbClsExtra = 0;
+	wcx.cbWndExtra = 0;
+	wcx.hInstance = hinstance;
+	wcx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcx.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcx.lpszMenuName = NULL;
+	wcx.lpszClassName = L"MainWClass";
+	wcx.hIconSm = (HICON)LoadImage(hinstance,
+		MAKEINTRESOURCE(5), IMAGE_ICON,
+		GetSystemMetrics(SM_CXSMICON),
+		GetSystemMetrics(SM_CYSMICON),
+		LR_DEFAULTCOLOR);
+
+	if (!RegisterClassEx(&wcx))
+	{
+		return 1;
+	}
 
 	//创建窗口
-	CreateWindow("MainWClass",)
+	hwnd = CreateWindow(L"MainWClass",
+		L"Hello",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		(HWND)NULL,
+		(HMENU)NULL,
+		hinstance,
+		(LPVOID)NULL);
+
+	if (!hwnd)
+	{
+		return 1;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while ((fGotMessage = GetMessage(&msg, (HWND)NULL, 0, 0)) != 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
 }
 
 
 //窗口过程函数，处理消息
 LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	return 0;
+	switch (uMsg)
+	{
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
 }
